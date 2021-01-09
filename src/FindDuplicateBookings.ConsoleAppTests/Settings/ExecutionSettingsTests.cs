3eb8b316461d91397feb11ff296f328bf481276f
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2021-01-09 10:36:11 +0530

    ExecutionSettingsTests

---
+++ src/FindDuplicateBookings.ConsoleAppTests/Settings/ExecutionSettingsTests.cs
@@ -0,0 +1,21 @@
+﻿using FindDuplicateBookings.Lib.Settings;
+using Xunit;
+
+namespace FindDuplicateBookings.ConsoleAppTests.Settings
+{
+    public class ExecutionSettingsTests
+    {
+        [Fact]
+        public void ExecutionSettingsModelTest()
+        {
+            // Arrange.
+            ExecutionSettings executionSettings = new()
+            {
+                ExecutionFrequency = 300
+            };
+
+            // Assert.
+            Assert.Equal(300, executionSettings.ExecutionFrequency);
+        }
+    }
+}

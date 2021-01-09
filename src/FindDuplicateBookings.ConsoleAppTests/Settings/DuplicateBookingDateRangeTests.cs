b47cb74904e98f11f6c19b3c84dfc3d1432cfe5f
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2021-01-09 09:31:20 +0530

    DuplicateBookingDateRangeTests

---
+++ src/FindDuplicateBookings.ConsoleAppTests/Settings/DuplicateBookingDateRangeTests.cs
@@ -0,0 +1,24 @@
+﻿using FindDuplicateBookings.Lib.Settings;
+using System;
+using Xunit;
+
+namespace FindDuplicateBookings.ConsoleAppTests.Settings
+{
+    public class DuplicateBookingDateRangeTests
+    {
+        [Fact]
+        public void DuplicateBookingDateRangeModelTest()
+        {
+            // Arrange.
+            DuplicateBookingDateRange duplicateBookingDateRange = new()
+            {
+                StartDate = DateTime.UtcNow.AddDays(-10),
+                EndDate = DateTime.UtcNow.AddDays(10)
+            };
+
+            // Assert.
+            Assert.Equal(DateTime.UtcNow.AddDays(-10).Date, duplicateBookingDateRange.StartDate.Date);
+            Assert.Equal(DateTime.UtcNow.AddDays(10).Date, duplicateBookingDateRange.EndDate.Date);
+        }
+    }
+}

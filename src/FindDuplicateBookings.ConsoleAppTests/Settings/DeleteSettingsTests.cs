4ab4e70d836986651ae426da3afcb585d797d859
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2021-01-09 11:16:20 +0530

    Unit tests for model and settings. (#2)

---
+++ src/FindDuplicateBookings.ConsoleAppTests/Settings/DeleteSettingsTests.cs
@@ -0,0 +1,34 @@
+﻿using FindDuplicateBookings.Lib.Settings;
+using Xunit;
+
+namespace FindDuplicateBookings.ConsoleAppTests.Settings
+{
+    public class DeleteSettingsTests
+    {
+        [Fact]
+        public void DeleteSettingsModelTest_True()
+        {
+            // Arrange.
+            DeleteSettings deleteSettings = new()
+            {
+                IsDeleteEnable = "true"
+            };
+
+            // Assert.
+            Assert.Equal("true", deleteSettings.IsDeleteEnable);
+        }
+
+        [Fact]
+        public void DeleteSettingsModelTest_False()
+        {
+            // Arrange.
+            DeleteSettings deleteSettings = new()
+            {
+                IsDeleteEnable = "false"
+            };
+
+            // Assert.
+            Assert.NotEqual("true", deleteSettings.IsDeleteEnable);
+        }
+    }
+}

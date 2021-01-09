55996bef49a6cf2a17f181b5939b389d4b47c76b
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2021-01-09 08:20:37 +0530

    Booking model tests.

---
+++ src/FindDuplicateBookings.ConsoleAppTests/Models/BookingTests.cs
@@ -0,0 +1,44 @@
+﻿using FindDuplicateBookings.Lib.Models;
+using Xunit;
+
+namespace FindDuplicateBookings.ConsoleAppTests.Models
+{
+    public class BookingTests
+    {
+        [Fact]
+        public void BookingModelTest()
+        {
+            // Arrange.
+            Booking booking = new()
+            {
+                AssignmentId = 15,
+                TestInsId = 102,
+                SessionId = 452
+            };
+
+            // Assert.
+            Assert.NotNull(booking);
+            Assert.Equal(15, booking.AssignmentId);
+            Assert.Equal(102, booking.TestInsId);
+            Assert.Equal(452, booking.SessionId);
+        }
+
+        [Fact]
+        public void BookingModelToStringTest()
+        {
+            // Arrange.
+            Booking booking = new()
+            {
+                AssignmentId = 15,
+                TestInsId = 102,
+                SessionId = 452
+            };
+
+            // Act.
+            string bookingString = booking.ToString();
+
+            // Assert.
+            Assert.Equal("AssignmentId 15, TestInsId 102, SessionId 452.", bookingString);
+        }
+    }
+}
